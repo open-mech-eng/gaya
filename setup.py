c5fd87fa4306534fd2bffa570a15975818ef9f0d
noce2 <20339399+noce2@users.noreply.github.com>
2021-07-25 08:21:56 +0100

    Update setup.py to show new package

--- setup.py
+++ setup.py
@@ -5,52 +5,56 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 HERE = Path(__file__).absolute().parent
 
-requirements = (HERE/"requirements.txt").read_text().split("\n")
+requirements = (HERE / "requirements.txt").read_text().split("\n")
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest', ]
+test_requirements = [
+    "pytest",
+]
 
 setup(
     author="noce2",
-    author_email='20339399+noce2@users.noreply.github.com',
-    python_requires='>=3.7',
+    author_email="20339399+noce2@users.noreply.github.com",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="An object-oriented and code-driven approach to engineering design and modelling.",
     entry_points={
-        'console_scripts': [
-            'gaya=gaya.cli:main',
+        "console_scripts": [
+            "gaya=gaya.cli:main",
         ],
     },
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='gaya',
-    name='gaya',
-    packages=find_packages(include=['gaya', 'gaya.*']),
+    keywords="gaya",
+    name="gaya",
+    packages=find_packages(include=["gaya", "gaya.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/open-mech-eng/gaya',
-    version='0.1.0',
+    url="https://github.com/quado-technologies/gaya",
+    version="0.1.0",
     zip_safe=False,
 )
